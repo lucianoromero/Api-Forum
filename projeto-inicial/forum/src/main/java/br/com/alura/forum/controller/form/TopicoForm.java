975d47ac8c6df6aa0edf84f8c11b6f3f5ff0a563
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2021-06-02 11:08:34 -0300

    Validacoes com o Bean Validation

--- projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,13 +1,22 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull
+	@NotEmpty
 	private String titulo;
+	@NotNull
+	@NotEmpty
 	private String mensagem;
+	@NotNull
+	@NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
@@ -38,7 +47,7 @@ public class TopicoForm {
 	 * @return Converte para um objeto do tipo Topico
 	 */
 	public Topico converter(CursoRepository cursoRepository) {
-        Curso curso = cursoRepository.findByNome(nomeCurso);		
+		Curso curso = cursoRepository.findByNome(nomeCurso);
 		return new Topico(titulo, mensagem, curso);
 
 	}
