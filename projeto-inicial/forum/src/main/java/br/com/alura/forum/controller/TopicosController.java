eb58b6a21d37948f2171be427eb2e2f6f2beb6ae
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2021-06-07 12:05:20 -0300

    Criando pesquisa por topico com ID. Detalhando topico

--- projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -29,7 +31,7 @@ public class TopicosController {
 
 	@Autowired
 	private CursoRepository cursoRepository;
-	
+
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 
@@ -45,11 +47,18 @@ public class TopicosController {
 
 	@PostMapping
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
-       Topico topico = form.converter(cursoRepository);
+		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-        
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+
+	@GetMapping("/{id}")
+	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+
+		return new DetalhesDoTopicoDto (topico);
 	}
 
 }
