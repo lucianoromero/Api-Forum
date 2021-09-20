3b778c35dcba7a675d84e1c6fa44056774547d7a
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-09-20 19:28:51 -0300

    Criando metodo de atualização dos topicos

---
+++ projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class AtualizacaoTopicoForm {
+
+	@NotNull
+	@NotEmpty
+	private String titulo;
+	
+	@NotNull
+	@NotEmpty
+	private String mensagem;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	/**
+	 * @param id
+	 * @param topicoRepository
+	 * @return Retorna o topico atualizado
+	 */
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		return topico;
+	}
+
+}
