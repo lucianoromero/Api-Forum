f78924c61fb13be0a2da804fa3439c34feb5c5ad
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-04 19:23:13 -0300

    Restringindo o acesso aos endpoints privado - inicio da implementacao de login

--- src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
@@ -22,7 +22,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	
     //Configuracoes de recursos estaticos(js, css, imagens, etc.)
