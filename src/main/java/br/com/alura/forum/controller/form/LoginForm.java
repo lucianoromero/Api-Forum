1e0830b49e89dc345645a9b4a9007b07fc6c99f6
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-08 16:46:06 -0300

    Implementando: Gerando token com JWT
    Implementação autenticação e gerando o token

--- src/main/java/br/com/alura/forum/controller/form/LoginForm.java
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 
 	private String email;
@@ -19,6 +21,10 @@ public class LoginForm {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
 	}
 
 }
