54fa0b0b68b411777d9cc75d3fabdfedd9987f82
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-09 16:45:28 -0300

    Implementando: Documentação da API com SpringFox Swagger Modulo2
    Realizando a documentação da API com o Swagger

---
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.alura.forum.modelo.Usuario;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(Usuario.class);
+                
+    }
+
+}
