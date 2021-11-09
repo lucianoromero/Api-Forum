dac62b0baa1c471af91a4cc023d5e73595088aea
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-11-09 16:59:14 -0300

    Implementando: Enviando token JWT da documentação do Swagger Modulo2
    Implementando para conseguir enviar o token dentro do Swagger

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,15 @@
 package br.com.alura.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.forum.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,8 +23,15 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
-                
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                                new ParameterBuilder()
+                                    .name("Authorization")
+                                    .description("Header para Token JWT")
+                                    .modelRef(new ModelRef("string"))
+                                    .parameterType("header")
+                                    .required(false)
+                                    .build()));               
     }
 
 }
